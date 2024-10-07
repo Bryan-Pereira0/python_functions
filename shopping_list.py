9a780929b3b1e956aeea30b49a2f50f580f1a585
bryanpereira <gfxmagicz@gmail.com>
2024-10-07 16:12:38 -0400

    Fixed shopping list

--- shopping_list.py
+++ shopping_list.py
@@ -1,36 +1,43 @@
 #task123
+def add_item(shoppinglist):
+    item = input("Enter the item you want to add: ")
+    shoppinglist.append(item)
+    print(f"{item} has been added to your list.")
+
+def remove_item(shoppinglist):
+    item_to_remove = input("What would you like to remove?")
+    if item_to_remove in shoppinglist:
+        shoppinglist.remove(item_to_remove)
+        print(f"{item_to_remove} has been removed from your list.")
+    else:
+        print("Item isn't in list.")
+
+def print_list(shoppinglist):
+    if len(shoppinglist) == 0:
+        print("The list is empty")
+    else:
+        if shoppinglist:
+            print("Your shopping list: ")
+            index = 1
+            for item in shoppinglist:
+                print(f"{index}. {item}")
+                index += 1
+
 def shopping_list():
-    shoppinglist = []
-    print("Hello what would you like to add or remove to your shopping list")
-    print("Use A to add or R to remove, use Q at anytime to quit and S to show your list!")
-    
-    
+    shoppinglist = []  
     while True:
+        print("Hello what would you like to add or remove to your shopping list")
+        print("Use A to add or R to remove, use Q at anytime to quit and S to show your list!")  
         choice = input("A, R, Q or, S?: ")
         if choice == 'A':
-            item = input("Enter the item you want to add: ")
-            shoppinglist.append(item)
-            print(f"{item} has been added to your list.")
+            add_item(shoppinglist)  
         elif choice == 'R':
-            item_to_remove = input("What would you like to remove?")
-            if item_to_remove in shoppinglist:
-                shoppinglist.remove(item_to_remove)
-                print(f"{item_to_remove} has been removed from your list.")
-            else:
-                print("Item isn't in list.")
+            remove_item(shoppinglist)
         elif choice == 'Q':
-            print("Thank you for using this list maker!")
-            break
+                print("Thank you for using this list maker!")
+                break
         elif choice == 'S':
-            if len(shoppinglist) == 0:
-                print("The list is empty")
-            else:
-                if shoppinglist:
-                    print("Your shopping list: ")
-                    index = 1
-                    for item in shoppinglist:
-                        print(f"{index}. {item}")
-                        index += 1
+                print_list(shoppinglist)
         else:
             print("You can't do that.")
             
